a3168d8ae06c5022642d32db4e59ddee19639480
idlesummer <09louisthebob@gmail.com>
2025-10-15 13:04:04 +0800

    Additional refactors

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils'
+
+export function TypographyH1({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h1
+      className={cn('scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl', className)}
+      {...props}
+    />
+  )
+}
+
+export function TypographyP({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) {
+  return (
+    <p
+      className={cn('leading-7 text-muted-foreground', className)}
+      {...props}
+    />
+  )
+}
